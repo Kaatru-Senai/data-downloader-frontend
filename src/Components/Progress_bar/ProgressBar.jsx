5cc9f6cfe362b4fabdbd51f0a62b5adb2e64550b
Hemnath0075 <hemnath5700@gmail.com>
2023-05-16 14:02:11 +0530

    FW03-14 progress bar functionality done

--- src/Components/Progress_bar/ProgressBar.jsx
+++ src/Components/Progress_bar/ProgressBar.jsx
@@ -1,18 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState } from 'react'
 import styles from './ProgressBar.module.css'
+import { useLocation } from 'react-router-dom';
 
 function ProgressBar() {
+  const location =useLocation();
+  const [progressStep,setProgressStep]=useState(0);
   useEffect(()=>{
-    
+    console.log(location.pathname);
+    switch(location.pathname){
+      case "/select-datasource":
+        setProgressStep(1);
+        break;
+      case "/select-dates":
+        setProgressStep(2);
+        break;
+      case "/select-devices":
+        setProgressStep(3);
+        break;
+      case "/select-datatype":
+        setProgressStep(4);
+        break;
+      case "/select-database":
+        setProgressStep(5);
+        break;
+    }
+    console.log(progressStep)
   },[])
   return (
     <div className={styles.ProgressBar}>
-      <ul>
-        <li className={styles.active}>Select Request</li>
-        <li className={styles.active}>Enter Job Id</li>
-        <li>Select Devices</li>
-        <li>Select Format</li>
-        <li>Download Data</li>
+      <ul className='flex flex-row-reverse'>
+        <li className={progressStep==5 && styles.active}>Select Database</li>
+        <li className={progressStep==4 && styles.active}>Select Format</li>
+        <li className={progressStep==3 && styles.active}>Select Devices</li>
+        <li className={progressStep==2 && styles.active}>Select Date Range</li>
+        <li className={progressStep==1 && styles.active}>Select DataSource</li>
       </ul>
     </div>
   )
--- src/Pages/DateSelection.jsx
+++ src/Pages/DateSelection.jsx
@@ -242,7 +242,7 @@ function DateSelection() {
       <div className="w-[80%] flex flex-row justify-between items-center mb-[2%] mt-[2%]">
         <button
           className="bg-[#DFDFDF] px-4 py-2 text-[#616161] font-semibold rounded-lg"
-          onClick={() => navigate("/select-request")}
+          onClick={() => navigate("/select-datasource")}
         >
           Back
         </button>
